ca46a6de0a3a5d773d2d8446a7616080fec1ee43
Laura Giovanna Fernandes Vattimo <laura.vattimo@einstein.br>
2025-05-28 12:03:25 -0300

    testei fazer um grafico usando ggplot e geom_histogram

---
+++ teste2.R
@@ -0,0 +1,8 @@
+
+install.packages("tidyr")
+install.packages("palmerpenguins")
+library(tidyr)
+library(palmerpenguins)
+install.packages("ggplot2")
+library(ggplot2)
+penguins %>% ggplot(aes(x=bill_length_mm, y=)) +geom_histogram()
